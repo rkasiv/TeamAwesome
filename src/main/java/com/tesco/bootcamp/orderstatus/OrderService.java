ee24b4a2242cf29f09fd666cdb126d22b75e1fc8
cx11 <rkasiv@gmail.com>
2017-01-11 13:30:46 +0000

    comment brnch

--- src/main/java/com/tesco/bootcamp/orderstatus/OrderService.java
+++ src/main/java/com/tesco/bootcamp/orderstatus/OrderService.java
@@ -2,14 +2,15 @@ package com.tesco.bootcamp.orderstatus;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by cx11 on 11/01/2017.
  */
 
-@Component
+@Service
 public class OrderService {
 
-
+// test commit
 
 }

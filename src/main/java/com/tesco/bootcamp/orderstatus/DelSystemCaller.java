78e0098c805a2bd7af8938217c0f2c38b59854ea
CattleRoyale <mslagter23@gmail.com>
2017-01-11 22:54:38 +0000

    Completed return latest event method

--- src/main/java/com/tesco/bootcamp/orderstatus/DelSystemCaller.java
+++ src/main/java/com/tesco/bootcamp/orderstatus/DelSystemCaller.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,18 +28,50 @@ public class DelSystemCaller {
         getEventsByParcelURL = "events/ghs/parcel?parcelId=";
     }
 
-    private String getParcelID(String orderID){
+    public TrackingEvent getLatestTrackingEvent(String orderID){
+        List<EventFromDelService> orderEvents = collectParcelID(orderID);
 
-        return "";
+        for (EventFromDelService event: orderEvents){
+            parcelID = event.getParcelID();
+        }
+
+        List<TrackingEvent> trackingEvents = collectTrackingEvents(parcelID);
+
+        TrackingEvent latestEvent = returnLatestTrackingEvent(trackingEvents);
+
+        return latestEvent;
+    }
+
+    private TrackingEvent returnLatestTrackingEvent(List<TrackingEvent> trackingEvents){
+        TrackingEvent latestEvent;
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        Date earliestDate;
+        try {
+            earliestDate = df.parse(trackingEvents.get(0).getEventDateTime());
+        } catch (Exception e){
+            throw new RuntimeException("Failed to parse earliestDate");
+        }
+        latestEvent = trackingEvents.get(0);
+        Date eventDate;
+
+        for (TrackingEvent event: trackingEvents){
+            try {
+                eventDate = df.parse(event.getEventDateTime());
+            }catch (Exception e) {
+
+                throw new RuntimeException("Failed to parse eventDate", e);
+            }
+
+            if (eventDate.after(earliestDate)){
+                latestEvent = event;
+            }
+        }
+
+        return latestEvent;
     }
 
 
-
-    public void setParcelID(String parcelID) {
-        this.parcelID = parcelID;
-    }
-
-    public List<EventFromDelService> collectParcelID(String orderID) {
+    private List<EventFromDelService> collectParcelID(String orderID) {
         RestTemplate restTemplate = new RestTemplate();
         StringBuilder sb = new StringBuilder();
         sb.append(deliveryServiceBaseURL);
@@ -61,7 +96,7 @@ public class DelSystemCaller {
         }
     }
 
-    public List<TrackingEvent> collectTrackingEvents(String parcelID) {
+    private List<TrackingEvent> collectTrackingEvents(String parcelID) {
         RestTemplate restTemplate = new RestTemplate();
         StringBuilder sb = new StringBuilder();
         sb.append(deliveryServiceBaseURL);
